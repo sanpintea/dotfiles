e03d447eecf30a7af0d39257e405778236c5678b
Takeshi Kawamitsu <kawamitsu.t@gmail.com>
2016-03-14 17:30:28 +0900

    add init.el file

---
+++ init.el
@@ -0,0 +1,74 @@
+;;;対応するカッコを強調表示
+(show-paren-mode t)
+;;;時刻表示
+(display-time)
+;;; ロードパスの追加
+(setq load-path (append
+                 '("~/.emacs.d"
+                   "~/.emacs.d/packages")
+                 load-path))
+;;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;; 現在行を目立たせる
+(global-hl-line-mode)
+
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;;; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; ウィンドウを透明にする
+;; アクティブウィンドウ／非アクティブウィンドウ（alphaの値で透明度を指定）
+(add-to-list 'default-frame-alist '(alpha . (0.85 0.85)))
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+(setq company-tern-property-marker "")
+(defun company-tern-depth (candidate)
+  "Return depth attribute for CANDIDATE. 'nil' entries are treated as 0."
+  (let ((depth (get-text-property 0 'depth candidate)))
+    (if (eq depth nil) 0 depth)))
+(add-hook 'js2-mode-hook 'tern-mode) ; 自分が使っているjs用メジャーモードに変える
+(add-to-list 'company-backends 'company-tern) ; backendに追加
+;;;(add-to-list 'company-backends '(company-tern :with company-dabbrev-code))
