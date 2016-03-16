44914a7f53e463abf76f82890c1276c38520b20b
Takeshi Kawamitsu <kawamitsu.t@gmail.com>
2016-03-16 23:14:00 +0900

    use emacs package manager el-get

--- init.el
+++ init.el
@@ -1,14 +1,27 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;;el-getの設定
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+
+;;; ロードパスの追加
+ (setq load-path (append
+                  '("~/.emacs.d/elpa"
+                    "~/.emacs.d/el-get"
+                    "~/.emacs.d/packages")
+load-path))
+
 ;;;対応するカッコを強調表示
 (show-paren-mode t)
 ;;;時刻表示
 (display-time)
-;;; ロードパスの追加
-
- (setq load-path (append
-                  '("~/.emacs.d/elpa"
-                    "~/.emacs.d/packages")
-load-path))
-
 ;;; Localeに合わせた環境の設定
 (set-locale-environment nil)
 (set-language-environment "Japanese")
@@ -22,7 +35,6 @@ load-path))
 (setq inhibit-startup-message t)
 ;; メニューバーを消す
 (menu-bar-mode -1)
-
 ;; ツールバーを消す
 ;; (tool-bar-mode -1)
 
@@ -64,6 +76,9 @@ load-path))
 ;; カーソルの点滅をやめる
 (blink-cursor-mode 0)
 
+;;; 右から左に読む言語に対応させないことで描画高速化
+(setq-default bidi-display-reordering nil)
+
 ;; 起動時のウィンドウサイズ、色などを設定
 (if (boundp 'window-system)
     (setq default-frame-alist
@@ -82,20 +97,41 @@ load-path))
                   default-frame-alist)))
 (setq initial-frame-alist default-frame-alist )
 
-
+;;パッケージ管理
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+
 
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize) ;; You might already have this line
 
+;;パッケージインストールリスト
+(el-get-bundle helm)
+(el-get-bundle ac-js2)
+(el-get-bundle company)
+(el-get-bundle popup)
+;;(el-get-bundle auto-complete)
+(el-get-bundle js2-mode)
+(el-get-bundle company-tern)
+(el-get-bundle anzu)
+(el-get-bundle diminish)
+
+;;パッケージ毎の設定
+(require 'diminish)
+
+(require 'anzu)
+;; (require 'auto-complete)
+;; (require 'auto-complete-config)    ; 必須ではないですが一応
+;; (global-auto-complete-mode t)
 
 (global-company-mode) ; 全バッファで有効にする 
+(add-hook 'ruby-mode-hook 'company-mode)
 (setq company-idle-delay 0.3) ; デフォルトは0.5
 (setq company-minimum-prefix-length 2) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
@@ -106,18 +142,12 @@ load-path))
   (let ((depth (get-text-property 0 'depth candidate)))
     (if (eq depth nil) 0 depth)))
 ;;(add-hook 'js2-mode-hook 'tern-mode) ; 自分が使っているjs用メジャーモードに変える
-;;(add-to-list 'company-backends 'company-tern) ; backendに追加
+(add-to-list 'company-backends 'company-tern) ; backendに追加
+(global-anzu-mode +1)
 
-(add-to-list 'company-backends '(company-tern :with company-dabbrev-code))
+(diminish 'anzu-mode "Az")
+(diminish 'company-mode "Comp")
 
-;;;(add-to-list 'company-backends '(company-tern :with company-dabbrev-code))
 
 
-;;(autoload 'js2-mode "js2" nil t)
-;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;;(add-hook 'after-init-hook 'global-company-mode)
-;;(add-hook 'ruby-mode-hook 'company-mode)
-
-;(autoload 'undo-tree-mode "undo-tree-mode" nil t)
-;;(require 'undo-tree)
