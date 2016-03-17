cac1de919f5cdf27222757600d5bc14c4d179963
Takeshi Kawamitsu <kawamitsu.t@gmail.com>
2016-03-17 23:39:36 +0900

    init.el 使用しないパッケージレシピの削除

--- init.el
+++ init.el
@@ -118,9 +118,10 @@ load-path))
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; (when (< emacs-major-version 24)
+;;   ;; For important compatibility libraries like cl-lib
+;;   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+;;   )
 
 
 (add-to-list 'package-archives
